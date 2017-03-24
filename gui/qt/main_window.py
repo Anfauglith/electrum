33a9406599de386fec9c35435c6d649def7bde39
Amon Engemann <amon.e@gmx.net>
2017-03-24 01:17:20 +0100

    fixed bug report dialogue

--- gui/qt/main_window.py
+++ gui/qt/main_window.py
@@ -509,8 +509,8 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
     def show_report_bug(self):
         msg = ' '.join([
             _("Please report any bugs as issues on github:<br/>"),
-            "<a href=\"https://github.com/spesmilo/electrum/issues\">https://github.com/spesmilo/electrum/issues</a><br/><br/>",
-            _("Before reporting a bug, upgrade to the most recent version of Electrum (latest release or git HEAD), and include the version number in your report."),
+            "<a href=\"https://github.com/anfauglith/electrum-iop/issues\">https://github.com/anfauglith/electrum-iop/issues</a><br/><br/>",
+            _("Before reporting a bug, upgrade to the most recent version of Electrum-IOP (latest release or git HEAD), and include the version number in your report."),
             _("Try to explain not only what the bug is, but how it occurs.")
          ])
         self.show_message(msg, title="Electrum - " + _("Reporting Bugs"))
